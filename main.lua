8911f02651c064ad7e98dd3cf65ad92030be4667
foxy <69487888+foxyyx@users.noreply.github.com>
2023-06-24 17:28:44 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,12 +1,11 @@
 function getTimeFormat(t)
     if (not t) then return error ('Error in argument #1. Set the timestamp') end
     local seconds, minutes, hours, days, months, years = math_floor (t / 1000), math_floor (t / 60000), math_floor (t / 3600000), math_floor (t / 86400000), math_floor (t / 2628000000), math_floor (t / 31540000000)
-    seconds, minutes = math_floor (seconds % 60), (minutes - (hours * 60))
-    return years..' ano(s), '..(months - (years * 12))..' mes(es) e '..(days - (years * 366))..' dia(s) - '..(hours - (days * 24))..' hora(s), '..minutes..' minuto(s) e '..seconds..' segundo(s)'
+    return years..' ano(s), '..(months - (years * 12))..' mes(es) e '..(days - (years * 366))..' dia(s) - '..(hours - (days * 24))..' hora(s), '..(minutes - (hours * 60))..' minuto(s) e '..math_floor (seconds % 60)..' segundo(s)'
 end
 
 function getTimeDifference (time)
-    if (not t) then return error ('Error in argument #1. Set the timestamp') end
+    if (not time) then return error ('Error in argument #1. Set the timestamp') end
     local time = getRealTime (time).timestamp
     local difference = math.abs (os.difftime (os.time (), time)/0.001)
     return getTimeFormat (difference), difference
