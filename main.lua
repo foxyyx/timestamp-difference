e691e38d9178f8165836f9de6671c8e4d43a1f34
foxy <69487888+foxyyx@users.noreply.github.com>
2023-06-25 13:18:31 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 
 local math_floor = math.floor
 
-function getTimeFormat(t)
+function getTimeFormat (t)
     if (not t) then return error ('Error in argument #1. Set the timestamp') end
     local seconds, minutes, hours, days, months, years = math_floor (t / 1000), math_floor (t / 60000), math_floor (t / 3600000), math_floor (t / 86400000), math_floor (t / 2628000000), math_floor (t / 31540000000)
     return years..' ano(s), '..(months - (years * 12))..' mes(es) e '..(days - (years * 366))..' dia(s) - '..(hours - (days * 24))..' hora(s), '..(minutes - (hours * 60))..' minuto(s) e '..math_floor (seconds % 60)..' segundo(s)'
